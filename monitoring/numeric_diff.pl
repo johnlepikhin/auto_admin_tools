3ae5ebc73a11d55d95b9e166800edeee6afeac5a
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2018-02-20 12:04:29 +0300

    Add -ignore, -no-clear. Bugfixes.

--- monitoring/numeric_diff.pl
+++ monitoring/numeric_diff.pl
@@ -7,20 +7,28 @@ use Carp qw(cluck croak confess);
 
 my $opt_sleep      = 1;
 my $opt_iterations = 2**62;
-my $opt_indent = 0;
+my $opt_indent     = 0;
 my $opt_command;
-my $opt_color = 0;
+my $opt_color    = 0;
+my $opt_no_clear = 0;
+my $opt_ignore;
 
 GetOptions(
-    's=f' => \$opt_sleep,
-    'n=i' => \$opt_iterations,
-    'c=s' => \$opt_command,
-    'i' => \$opt_indent,
-    'color' => \$opt_color,
+    's=f'      => \$opt_sleep,
+    'n=i'      => \$opt_iterations,
+    'c=s'      => \$opt_command,
+    'i'        => \$opt_indent,
+    'no-clear' => \$opt_no_clear,
+    'color'    => \$opt_color,
+    'ignore=s' => \$opt_ignore,
 ) || croak "Cannot parse command line arguments";
 
 if ( !defined $opt_command ) {
     croak "-c 'command' is expected";
+}
+
+if ( !defined $opt_ignore ) {
+    $opt_ignore = "(?!)";
 }
 
 my @prev;
@@ -30,37 +38,43 @@ for ( 1 .. $opt_iterations ) {
     { local $/ = q{}; $output = <$fh>; }
     close $fh;
 
-    my @new = split m{([-+]?[0-9]*[.]?[0-9]+)}, $output;
-    if (scalar @prev == scalar @new) {
-        print "\x1b[3J\x1b[H\x1b[2J";
-        for my $pos (0..$#new) {
-            if ($new[$pos] =~ m{[-+]?[0-9]*[.]?[0-9]+}) {
-                my $v = $new[$pos] -$prev[$pos];
-                if ($v > 0) {
-                    $v = "+$v";
-                }
-                if ($opt_color) {
-                    if ($v > 0) {
-                        print "\x1b[0;32m";
-                    } elsif ($v < 0) {
-                        print "\x1b[0;31m";
+    if ( defined $output ) {
+        my @new = split m{($opt_ignore|[+-]?\d*[.]?\d+)}, $output;
+        if ( scalar @prev == scalar @new ) {
+            if ( !$opt_no_clear ) {
+                print "\x1b[3J\x1b[H\x1b[2J";
+            }
+            for my $pos ( 0 .. $#new ) {
+                if ( $new[$pos] =~ m{^$opt_ignore$} ) {
+                    print $new[$pos];
+                } elsif ( $new[$pos] =~ m{[+-]?\d*[.]?\d+} ) {
+                    my $v = $new[$pos] - $prev[$pos];
+                    if ( $v > 0 ) {
+                        $v = "+$v";
                     }
-                }
-                if ($opt_indent) {
-                    my $len = length $new[$pos];
-                    printf "% ${len}s", $v;
+                    if ($opt_color) {
+                        if ( $v > 0 ) {
+                            print "\x1b[0;32m";
+                        } elsif ( $v < 0 ) {
+                            print "\x1b[0;31m";
+                        }
+                    }
+                    if ($opt_indent) {
+                        my $len = length $new[$pos];
+                        printf "% ${len}s", $v;
+                    } else {
+                        print $v;
+                    }
+                    if ( $opt_color && $v ) {
+                        print "\x1b[0m";
+                    }
                 } else {
-                    print $v;
+                    print $new[$pos];
                 }
-                if ($opt_color && $v) {
-                    print "\x1b[0m";
-                }
-            } else {
-                print $new[$pos];
             }
         }
+        @prev = @new;
     }
-    @prev = @new;
 
     select undef, undef, undef, $opt_sleep;
 }
